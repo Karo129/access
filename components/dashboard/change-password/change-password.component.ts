2bb730e6784167bfdbcdc12908d607eba5a27059
Karolina <karo129@wp.pl>
2023-04-27 16:54:29 +0200

    change-password

--- components/dashboard/change-password/change-password.component.ts
+++ components/dashboard/change-password/change-password.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css']
 })
-export class ChangePasswordComponent implements OnInit {
+export class ChangePasswordComponent {
+  oldPasswordFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(3)])
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- components/dashboard/questions/questions.component.ts
+++ components/dashboard/questions/questions.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
-export class QuestionsComponent implements OnInit {
+export class QuestionsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
