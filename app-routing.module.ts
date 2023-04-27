2f24f0eeb801678d45647ea1c75b8b99c2b6368f
Karolina <karo129@wp.pl>
2023-04-27 12:34:08 +0200

    changes in routing

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -4,13 +4,22 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { RegisterComponent } from './components/homePage/register/register.component';
 import { LoginComponent } from './components/homePage/login/login.component';
 import { ChallengeComponent } from './components/homePage/challenge/challenge.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuestionsComponent } from './components/dashboard/questions/questions.component';
+import { ChangePasswordComponent } from './components/dashboard/change-password/change-password.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/challenge', pathMatch: 'full'},
-  { path: 'challenge', component: ChallengeComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent},
-  { path: '**', redirectTo: '/challenge', pathMatch: 'full' }
+  { path: '', redirectTo: '/home-page', pathMatch: 'full'},
+  { path: 'home-page', component: HomePageComponent, children: [
+    { path: '', component: ChallengeComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent},
+  ]},
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: '', component: QuestionsComponent},
+    { path: 'changePassword', component: ChangePasswordComponent}
+  ]},
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
 @NgModule({
